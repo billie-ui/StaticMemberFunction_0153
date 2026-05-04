08ba478d233db1ba6d419d925b563c4e30493d85
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-04 16:01:22 +0700

    tambah constructor Mahasiswa

---
+++ constructor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa
+{
+    public:
+        Mahasiswa(); // Constructor
+};
+
+Mahasiswa::Mahasiswa()
+{
+    cout << "constructor Terpanggil" << endl; //definisi constructor
+};
